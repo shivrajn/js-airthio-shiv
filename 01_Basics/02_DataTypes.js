6b3db960885df35d9ad1f8e25967a4e502476536
Shivaraj nalawade <130443700+shivrajn@users.noreply.github.com>
2025-07-19 12:06:19 +0000

    Data types learning

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+ "user strict"; // treat all js code as newer version  
+
+ // alert (3+3) // we are using node js // not browser 
+
+ console.log(3+3) 
+ console.log("shivraj");
+ 
+ 
+let name = "shivaraj"
+let Age =19
+let state = "Null"
+
+
+// Number = 2 to power 53
+// String = ""
+//Biging = big value
+// Null = Object type , represatative of empty // stand alone value
+//Undefined = empty 
+//Boolean = true/ false
+//symabol - unique value represent 
+
+console.log(typeof undefined);
