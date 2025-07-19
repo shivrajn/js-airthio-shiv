c7546e61912c6ff9c00eb42620c8c632311de0af
Shivaraj nalawade <130443700+shivrajn@users.noreply.github.com>
2025-07-19 12:07:14 +0000

    Variables learning

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountid =19961998
+let userEmail =  "shivraj@gmail.com"
+var userpassword = "7896542"
+
+/* prepared not use to var 
+because of issue in block scope and functional scope*/
+
+let userEmail2 = "rutu@google.com"
+userpassword ="shiv785"
+console.table([userEmail, userpassword,accountid]);
+
