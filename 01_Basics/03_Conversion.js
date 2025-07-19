5f9ba831dc3598fa47b956d74f221138f2fb6052
Shivaraj nalawade <130443700+shivrajn@users.noreply.github.com>
2025-07-19 13:39:45 +0000

    Data Type Convsersion

---
+++ 01_Basics/03_Conversion.js
@@ -0,0 +1,11 @@
+let Age = "29Shiv";
+console.log(Age)
+let StringAge = Number(Age)
+console.log(StringAge)
+
+console.log(typeof StringAge);
+
+let StringBoolean = Boolean (StringAge)
+
+console.log(StringBoolean);
+
