06a045fc4c5abfcda14de5d4d02389573eb94c6f
Shivaraj nalawade <130443700+shivrajn@users.noreply.github.com>
2025-07-21 04:13:07 +0000

    Coversion Operaters

--- 01_Basics/03_Conversion.js
+++ 01_Basics/03_Conversion.js
@@ -1,11 +1,41 @@
-let Age = "29Shiv";
-console.log(Age)
-let StringAge = Number(Age)
-console.log(StringAge)
+// let Age = "29Shiv";
+// console.log(Age)
+// let StringAge = Number(Age)
+// console.log(StringAge)
 
-console.log(typeof StringAge);
+// console.log(typeof StringAge);
 
-let StringBoolean = Boolean (StringAge)
+// let StringBoolean = Boolean (StringAge)
 
-console.log(StringBoolean);
+// console.log(StringBoolean);
 
+//**********************Operatios*****************//
+
+// let Str1 = 1 
+// let Str2 = 2
+// let Str3 = Str1 + Str2 
+
+// console.log(Str3);
+// console.log( 1 + "2")
+// console.log("1" + 2);
+// console.log("1" + "2" + 2);
+// console.log( 1 + 2 +"2");
+
+
+// let gameChanger = 100 + 1
+// ++gameChanger
+// console.log(gameChanger);
+
+// Increment prefix & postfix 
+
+let x = 3;
+y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+ b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
